5f76126b4f379fd872e21ba150c3bfce86cf9333
Francisco Palá <francisco.palab@gmail.com>
2022-05-11 10:59:39 +0200

    deployed streamlit app with time series

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import streamlit as st
 import pandas as pd
-from 
+import pathlib
+from utils.funcs import *
 import plotly.express as px
 
+cwd = pathlib.Path.cwd()
 
 st.set_page_config(
     page_title='Covid-19 Dashboard Spain',
     layout = 'wide',
 )
 
-data = pd.read_csv('../data/covid_19_spain.csv', sep = ';')
+data = pd.read_csv(cwd / 'data/covid_19_spain.csv', sep = ';')
+
 age_series = get_sma7_by_age(data)
 fig = px.line(
     age_series, 
@@ -20,3 +23,41 @@ fig = px.line(
     width=1600,
     height=500,
     title = 'Daily Cases of Covid-19 by Age in Spain, 7-day Simple Moving Average')
+
+st.plotly_chart(fig, use_container_width=True)
+
+fig = px.line(
+    age_series, 
+    x="date", 
+    y="dailyHospitalizations", 
+    color='age',
+    template = 'simple_white',
+    width=1600,
+    height=500,
+    title = 'Daily Hospitalizations of Covid-19 by Age in Spain, 7-day Simple Moving Average')
+
+st.plotly_chart(fig, use_container_width=True)
+
+fig = px.line(
+    age_series, 
+    x="date", 
+    y="dailyICU", 
+    color='age',
+    template = 'simple_white',
+    width=1600,
+    height=500,
+    title = 'Daily ICU Admissions of Covid-19 by Age in Spain, 7-day Simple Moving Average')
+
+st.plotly_chart(fig, use_container_width=True)
+
+fig = px.line(
+    age_series, 
+    x="date", 
+    y="dailyDeaths", 
+    color='age',
+    template = 'simple_white',
+    width=1600,
+    height=500,
+    title = 'Daily Deaths of Covid-19 by Age in Spain, 7-day Simple Moving Average')
+
+st.plotly_chart(fig, use_container_width=True)
